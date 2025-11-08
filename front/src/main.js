86210d0a4a9f67b45d691dc181db818402f4d441
AdlayLevy <al@setpay.mx>
2025-11-07 19:11:24 -0600

    Login configuration

--- front/src/main.js
+++ front/src/main.js
@@ -4,8 +4,12 @@ import { createApp } from "vue";
 import PrimeVue from "primevue/config";
 import Aura from "@primeuix/themes/aura";
 import App from "./App.vue";
+import apiClient from "./api/axios";
 
 const app = createApp(App);
+
+app.config.globalProperties.$api = apiClient
+
 app.use(PrimeVue, {
   theme: {
     preset: Aura,
