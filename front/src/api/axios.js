43bf78041b5f3862e94c7d8d2c37de1cb8276f80
AdlayLevy <al@setpay.mx>
2025-11-10 17:13:12 -0600

    Update axios configuration

--- front/src/api/axios.js
+++ front/src/api/axios.js
@@ -22,4 +22,15 @@ apiClient.interceptors.request.use(
   }
 );
 
+apiClient.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      localStorage.removeItem("accessToken");
+      window.location.href = "/login";
+    }
+    return Promise.reject(error);
+  }
+);
+
 export default apiClient;
